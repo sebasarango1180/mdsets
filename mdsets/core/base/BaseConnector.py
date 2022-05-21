f71959faea3f83d96c6fb0ec45da387b7c18b225
Sebastian Arango <sebasarango1180@gmail.com>
2022-05-21 13:11:44 -0500

    Updated BaseConnector functions

--- mdsets/core/base/BaseConnector.py
+++ mdsets/core/base/BaseConnector.py
@@ -4,15 +4,24 @@ from abc import ABC, abstractmethod
 class BaseConnector(ABC):
     """
     Class intended to define the downloading and uploading mechanisms that
-    every source needs to follow to be consistent with the project.
+    every connector needs to follow to be consistent with the project.
     """
 
-    def __init__(self, source, destination):
+    def __init__(self):
         pass
 
     @abstractmethod
     def download(self):
         pass
 
+    @abstractmethod
     def upload(self):
+        pass
+
+    @abstractmethod
+    def _authenticate(self):
+        pass
+
+    @abstractmethod
+    def _get_connection(self):
         pass
